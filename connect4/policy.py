2fc86ef0349b66772209a076ee6b6a198b12ff08
Raphael Michel <mail@raphaelmichel.de>
2017-09-15 10:09:16 +0200

    3-layer NN

--- connect4/policy.py
+++ connect4/policy.py
@@ -8,12 +8,14 @@ class PolicyNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.affine1 = nn.Linear(BOARD_COLS, 128)
-        self.affine2 = nn.Linear(128, BOARD_COLS)
+        self.affine2 = nn.Linear(128, 128)
+        self.affine3 = nn.Linear(128, BOARD_COLS)
 
         self.saved_actions = []
         self.rewards = []
 
     def forward(self, x):
         x = F.relu(self.affine1(x))
-        action_scores = self.affine2(x)
+        x = self.affine2(x)
+        action_scores = self.affine3(x)
         return F.softmax(action_scores)
