7fab3aa8f67e7b8da6ee737e30f647591a6f98e0
Raphael Michel <mail@raphaelmichel.de>
2017-09-15 10:34:48 +0200

    Smaller NN

--- connect4/policy.py
+++ connect4/policy.py
@@ -7,12 +7,9 @@ from connect4.board import BOARD_COLS, BOARD_ROWS
 class PolicyNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.affine1 = nn.Linear(BOARD_COLS * BOARD_ROWS, 128)
-        self.affine2 = nn.Linear(128, 128)
-        self.affine3 = nn.Linear(128, BOARD_COLS)
-
-        self.saved_actions = []
-        self.rewards = []
+        self.affine1 = nn.Linear(BOARD_COLS * BOARD_ROWS, 64)
+        self.affine2 = nn.Linear(64, 64)
+        self.affine3 = nn.Linear(64, BOARD_COLS)
 
     def forward(self, x):
         x = F.relu(self.affine1(x))
