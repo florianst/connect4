4d2da4f1f2cc516a60dfdcc33fd5ade41bac1912
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2017-09-21 15:31:29 +0200

    reorganize and use matrix operations instead of nested for loops

--- connect4/readFromImage.py
+++ connect4/readFromImage.py
@@ -9,9 +9,9 @@ import numpy as np
 
 
 def print_runtime(descr=""):
-    global starttime
-    print(descr, "took", datetime.now()-starttime)
-    starttime = datetime.now()
+    global start_time
+    print(descr, "took", datetime.now()-start_time)
+    start_time = datetime.now()
 
 
 def read_file(relpath):
@@ -71,6 +71,68 @@ def treshold_brightness(x, thresh):
     x[x >= thresh] = 1
     return x
 
+
+# def mirror_board(b, axis = 1):
+#     b = np.flip(b, axis)
+#     return b
+
+
+def find_corners(b, margin = 40, dimensions = [350, 300]):
+    # mirror board
+    bM = np.fliplr(b)
+    topleft = [b.shape[0], b.shape[1]]
+    for x in range(b.shape[0]):
+        for y in range(b.shape[1]):
+            if (b[x, y] > 0 and x + y < topleft[0] + topleft[1]):
+                topleft[0] = x
+                topleft[1] = y
+
+    botright = [0, 0]
+    for x in range(b.shape[0]):
+        for y in range(b.shape[1]):
+            if (b[x, y] > 0 and x + y > botright[0] + botright[1]):
+                botright[0] = x
+                botright[1] = y
+
+    # bottom-left and top-right can be obtained in the same way from the board which is mirrored on the y-axis:
+    topright = [bM.shape[0], bM.shape[1]]
+    for x in range(bM.shape[0]):
+        for y in range(bM.shape[1]):
+            if (bM[x, y] > 0 and x + y < topright[0] + topright[1]):
+                topright[0] = x
+                topright[1] = y
+    # invert mirroring
+    topright[1] = bM.shape[1] - topright[1]
+
+    botleft = [0, 0]
+    for x in range(bM.shape[0]):
+        for y in range(bM.shape[1]):
+            if (bM[x, y] > 0 and x + y > botleft[0] + botleft[1]):
+                botleft[0] = x
+                botleft[1] = y
+    # invert mirroring
+    botleft[1] = bM.shape[1] - botleft[1]
+
+    src = np.array([[margin, margin], [margin, dimensions[1]], [dimensions[0], dimensions[1]], [dimensions[0], margin]])
+    dst = np.array([topleft[::-1], botleft[::-1], botright[::-1], topright[::-1]])
+    return src, dst, dimensions, margin
+
+
+def generate_discrete_boardconfig(num_cols, num_rows, treshold=1e-3):
+    # set up grid to extract colour information
+    grid = np.array([[50 * (i + 1), 50 * (num_rows - j)] for i in range(num_cols) for j in range(num_rows)])
+    grid = np.fliplr(grid)
+
+    # extract colour information
+    discrete_board = np.ones([num_rows, num_cols])
+    is_yellow = np.reshape(board_yellow_warped_smoothed[grid[:, 0], grid[:, 1]] > treshold, (6, 7), order='F')
+    is_red = np.reshape(board_red_warped_smoothed[grid[:, 0], grid[:, 1]] > treshold, (6, 7), order='F')
+
+    discrete_board[is_yellow] = 0
+    discrete_board[is_red] = 2
+    return discrete_board
+
+
 def is_valid(boardconfig):
     for row in range(6-1):
         for col in range(7):
@@ -83,6 +145,10 @@ red    = [156,10,25]
 white  = [150,148,138]
 blue   = [12,11,69]
 tolerance = 50
+
+# board configuration
+BOARD_COLS = 7
+BOARD_ROWS = 6
 
 board = read_file("../board_images/board1.jpg")
 
@@ -101,7 +167,7 @@ board_yellow = gaussian(board_yellow, sigma=2)
 board_red    = gaussian(board_red, sigma=2)
 board_white  = gaussian(board_white, sigma=2)
 
-#cut away low values below 95% brightness (i.e. increase contrast)
+# cut away low values below 95% brightness (i.e. increase contrast)
 board_total = board_yellow + board_red + board_white
 thresh = np.percentile(board_total, 95)
 
@@ -109,85 +175,26 @@ for board_current in [board_yellow, board_red, board_white]:
     board_current = treshold_brightness(board_current, thresh)
 
 board_total = board_yellow+board_red+board_white
-board_mirrored = np.fliplr(board_total)
+# board_mirrored = np.fliplr(board_total)
 
-# for viewing angles which are not too steep, it should be sufficient to look for the bright points with smallest and biggest coordinate sum and treat these as top-left / bottom-right corner:
-topleft = [board_total.shape[0], board_total.shape[1]]
-for x in range(board_total.shape[0]):
-    for y in range(board_total.shape[1]):
-        if (board_total[x, y]>0 and x+y<topleft[0]+topleft[1]):
-            topleft[0] = x
-            topleft[1] = y
-
-botright = [0, 0]
-for x in range(board_total.shape[0]):
-    for y in range(board_total.shape[1]):
-        if (board_total[x, y]>0 and x+y>botright[0]+botright[1]):
-            botright[0] = x
-            botright[1] = y
-
-# bottom-left and top-right can be obtained in the same way from the board which is mirrored on the y-axis:
-topright = [board_mirrored.shape[0], board_mirrored.shape[1]]
-for x in range(board_mirrored.shape[0]):
-    for y in range(board_mirrored.shape[1]):
-        if (board_mirrored[x, y]>0 and x+y<topright[0]+topright[1]):
-            topright[0] = x
-            topright[1] = y
-# invert mirroring
-topright[1] = board_mirrored.shape[1]-topright[1]
-
-botleft = [0, 0]
-for x in range(board_mirrored.shape[0]):
-    for y in range(board_mirrored.shape[1]):
-        if (board_mirrored[x, y]>0 and x+y>botleft[0]+botleft[1]):
-            botleft[0] = x
-            botleft[1] = y
-# invert mirroring
-botleft[1] = board_mirrored.shape[1]-botleft[1]
-
-# now, add constant margin and warp to square grid (image rectification using scikit-image):
-margin = 40
-dimensions = [350, 300] # this should be in the ratio 7:6 due to the board configuration
-
-# src and dest are a bit counter-intuitive...
-src = np.array([[margin, margin], [margin, dimensions[1]], [dimensions[0], dimensions[1]], [dimensions[0], margin]])
-dst = np.array([topleft[ : :-1],  botleft[ : :-1],         botright[ : :-1],               topright[ : :-1]]) # coordinates have to be flipped here - TODO: Why??
+src, dst, dimensions, margin = find_corners(board_total)
 
 transf = tf.ProjectiveTransform()
-transf.estimate(src, dst) # look for transformation transforming src to dst, i.e. the inverse transformation from dst to src
-board_total_warped = tf.warp(board_total, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
+# look for transformation transforming src to dst, i.e. the inverse transformation from dst to src
+transf.estimate(src, dst)
+board_total_warped  = tf.warp(board_total, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
 board_yellow_warped = tf.warp(board_yellow, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
-board_red_warped = tf.warp(board_red, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
-
-
-# set up grid to extract colour information
-grid = np.array([[50*(i+1), 50*(6-j)] for i in range(7) for j in range(6)])
-
-# extract colour information
-boardconfig = np.ones([6,7]) # six rows, seven columns                                    #1 for free slot (white)
+board_red_warped    = tf.warp(board_red, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
 
 # add an extra Gaussian smoothing in case the spots don't end up perfectly on the grid points:
 board_yellow_warped_smoothed = gaussian(board_yellow_warped, sigma=4)
 board_red_warped_smoothed    = gaussian(board_red_warped,    sigma=4)
 
-# plt.figure(figsize=(17,5))
-# plt.subplot(121)
-# plt.imshow(board_yellow_warped_smoothed, cmap='gray')
-# plt.plot(grid[:,0], grid[:,1], '.r')
-# plt.xlim(0,dimensions[0]+margin)
-# plt.ylim(dimensions[1]+margin,0)
-# plt.subplot(122)
-# plt.imshow(board_red_warped_smoothed, cmap='gray')
-# plt.plot(grid[:,0], grid[:,1], '.r')
-# plt.xlim(0,dimensions[0]+margin)
-# plt.ylim(dimensions[1]+margin,0)
-# plt.show()
+boardconfig = generate_discrete_boardconfig(BOARD_COLS, BOARD_ROWS)
 
-for i, pos in enumerate(grid):
-    if (board_yellow_warped_smoothed[pos[1], pos[0]] > 1e-3): boardconfig[i%6, i//6] = 0 #0 for yellow
-    elif (board_red_warped_smoothed[pos[1], pos[0]]  > 1e-3): boardconfig[i%6, i//6] = 2 #2 for red
 
-if (is_valid(boardconfig)):
+if is_valid(boardconfig):
+    print_runtime(descr="Total Run")
     print("Valid game configuration extracted:")
     # draw board and original photograph for comparison:
     colormap = ListedColormap(['gold', 'white', 'red'])
