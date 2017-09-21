68f2f3964a31f2b9024f0781eba5443c7aeb4d67
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2017-09-21 17:26:09 +0200

    clean

--- connect4/readFromImage.py
+++ connect4/readFromImage.py
@@ -1,6 +1,6 @@
 from skimage import io
 from skimage.filters import gaussian, laplace, median
-from skimage.morphology import disk
+# from skimage.morphology import disk
 from skimage import transform as tf
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -10,7 +10,7 @@ import numpy as np
 
 def print_runtime(descr=""):
     global start_time
-    print(descr, "took", datetime.now()-start_time)
+    print(descr, "took", datetime.now() - start_time)
     start_time = datetime.now()
 
 
@@ -19,11 +19,12 @@ def read_file(relpath):
     return board
 
 
-def extract_color(im, col, tol, sup = None):
+def extract_color(im, col, tol, sup=None):
     ix = np.sum((np.fabs(im - col * np.ones_like(im)) <= tol), 2) == 3
     cCube = np.zeros_like(im[:, :, 0])
     cCube[ix] = 1
-    if sup is not None: cCube[sup] = 0
+    if sup is not None:
+        cCube[sup] = 0
     cCube = cCube.astype('uint8')
     return cCube
 
@@ -52,14 +53,14 @@ def clean_white_board(bb, wb):
     # take lines of 45 degree angle to avoid Bresenham algorithm etc.
     for offset in range(bb.shape[1] - bb.shape[0]):  # bl->tr
         for x in range(bb.shape[0]):
-            if (bb[bb.shape[0] - 1 - x, x] == 1):
+            if bb[bb.shape[0] - 1 - x, x] == 1:
                 break
             else:
                 wb[bb.shape[0] - 1 - x, x + offset] = 0
 
     for offset in range(bb.shape[1] - bb.shape[0]):  # br->tl
         for x in range(bb.shape[0]):
-            if (bb[bb.shape[0] - x - 1, x] == 1):
+            if bb[bb.shape[0] - x - 1, x] == 1:
                 break
             else:
                 wb[bb.shape[0] - 1 - x, bb.shape[1] - 1 - x - offset] = 0
@@ -72,25 +73,20 @@ def treshold_brightness(x, thresh):
     return x
 
 
-# def mirror_board(b, axis = 1):
-#     b = np.flip(b, axis)
-#     return b
-
-
-def find_corners(b, margin = 40, dimensions = [350, 300]):
+def find_corners(b, margin=40, dimensions=[350, 300]):
     # mirror board
     bM = np.fliplr(b)
     topleft = [b.shape[0], b.shape[1]]
     for x in range(b.shape[0]):
         for y in range(b.shape[1]):
-            if (b[x, y] > 0 and x + y < topleft[0] + topleft[1]):
+            if b[x, y] > 0 and x + y < topleft[0] + topleft[1]:
                 topleft[0] = x
                 topleft[1] = y
 
     botright = [0, 0]
     for x in range(b.shape[0]):
         for y in range(b.shape[1]):
-            if (b[x, y] > 0 and x + y > botright[0] + botright[1]):
+            if b[x, y] > 0 and x + y > botright[0] + botright[1]:
                 botright[0] = x
                 botright[1] = y
 
@@ -98,7 +94,7 @@ def find_corners(b, margin = 40, dimensions = [350, 300]):
     topright = [bM.shape[0], bM.shape[1]]
     for x in range(bM.shape[0]):
         for y in range(bM.shape[1]):
-            if (bM[x, y] > 0 and x + y < topright[0] + topright[1]):
+            if bM[x, y] > 0 and x + y < topright[0] + topright[1]:
                 topright[0] = x
                 topright[1] = y
     # invert mirroring
@@ -107,7 +103,7 @@ def find_corners(b, margin = 40, dimensions = [350, 300]):
     botleft = [0, 0]
     for x in range(bM.shape[0]):
         for y in range(bM.shape[1]):
-            if (bM[x, y] > 0 and x + y > botleft[0] + botleft[1]):
+            if bM[x, y] > 0 and x + y > botleft[0] + botleft[1]:
                 botleft[0] = x
                 botleft[1] = y
     # invert mirroring
@@ -134,38 +130,40 @@ def generate_discrete_boardconfig(num_cols, num_rows, treshold=1e-3):
 
 
 def is_valid(boardconfig):
-    for row in range(6-1):
+    for row in range(6 - 1):
         for col in range(7):
-            if (boardconfig[row, col] == 1 and boardconfig[row+1, col] != 1): return False
+            if boardconfig[row, col] == 1 and boardconfig[row + 1, col] != 1:
+                return False
     return True
 
+
 # reference colours
-yellow = [193,155,17]
-red    = [156,10,25]
-white  = [150,148,138]
-blue   = [12,11,69]
+yellow = [193, 155, 17]
+red = [156, 10, 25]
+white = [150, 148, 138]
+blue = [12, 11, 69]
 tolerance = 50
 
 # board configuration
 BOARD_COLS = 7
 BOARD_ROWS = 6
 
-board = read_file("../board_images/board1.jpg")
+board = read_file("../board_images/board8.jpg")
 
 # keep track of script runtime for different tasks
 start_time = datetime.now()
 
 board_yellow = extract_color(board, yellow, tolerance)
-board_red    = extract_color(board, red, tolerance, board_yellow)
-board_white  = extract_color(board, white, tolerance, board_yellow + board_red)
-board_blue   = extract_color(board, blue, tolerance / 2, board_yellow + board_red + board_white)
+board_red = extract_color(board, red, tolerance, board_yellow)
+board_white = extract_color(board, white, tolerance, board_yellow + board_red)
+board_blue = extract_color(board, blue, tolerance / 2, board_yellow + board_red + board_white)
 
 board_white = clean_white_board(board_blue, board_white)
 
 # add Gaussian smoothing
 board_yellow = gaussian(board_yellow, sigma=2)
-board_red    = gaussian(board_red, sigma=2)
-board_white  = gaussian(board_white, sigma=2)
+board_red = gaussian(board_red, sigma=2)
+board_white = gaussian(board_white, sigma=2)
 
 # cut away low values below 95% brightness (i.e. increase contrast)
 board_total = board_yellow + board_red + board_white
@@ -174,21 +172,20 @@ thresh = np.percentile(board_total, 95)
 for board_current in [board_yellow, board_red, board_white]:
     board_current = treshold_brightness(board_current, thresh)
 
-board_total = board_yellow+board_red+board_white
+board_total = board_yellow + board_red + board_white
 # board_mirrored = np.fliplr(board_total)
 
 src, dst, dimensions, margin = find_corners(board_total)
 
 transf = tf.ProjectiveTransform()
-# look for transformation transforming src to dst, i.e. the inverse transformation from dst to src
 transf.estimate(src, dst)
-board_total_warped  = tf.warp(board_total, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
-board_yellow_warped = tf.warp(board_yellow, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
-board_red_warped    = tf.warp(board_red, transf, output_shape=(dimensions[1]+margin, dimensions[0]+margin))
+board_total_warped = tf.warp(board_total, transf, output_shape=(dimensions[1] + margin, dimensions[0] + margin))
+board_yellow_warped = tf.warp(board_yellow, transf, output_shape=(dimensions[1] + margin, dimensions[0] + margin))
+board_red_warped = tf.warp(board_red, transf, output_shape=(dimensions[1] + margin, dimensions[0] + margin))
 
 # add an extra Gaussian smoothing in case the spots don't end up perfectly on the grid points:
 board_yellow_warped_smoothed = gaussian(board_yellow_warped, sigma=4)
-board_red_warped_smoothed    = gaussian(board_red_warped,    sigma=4)
+board_red_warped_smoothed = gaussian(board_red_warped, sigma=4)
 
 boardconfig = generate_discrete_boardconfig(BOARD_COLS, BOARD_ROWS)
 
